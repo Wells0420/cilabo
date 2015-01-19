47e1b7d05475cb1091ef1879bc90b73c4dbd318b
Wells <lohaswells@gmail.com>
2015-01-20 03:52:05 +0800

    ＝ ＝

--- javascripts/scrollHelp.js
+++ javascripts/scrollHelp.js
@@ -10,9 +10,6 @@
 	scrollHelp.prototype = {
 	    init: function () {
 	        var _this = this;
-	  //       if(document.attachEvent){ 
-			// 	document.attachEvent('onmousewheel',scrollHander); 
-			// }else{
             _this.win.onmousewheel = function (e) {
             	// clearInterval(_this.intervalID);
             	if(!$(window).is(":animated")){
@@ -31,23 +28,6 @@
 	        }else{
 	        	var tar = _this.win.scrollTop + _this.step * (_this.upOrDown === "up" ? -1 : 1);
 	        }
-	        // console.log(document.documentElement.scrollTop);
-	        // console.log(tar);
-	        // _this.intervalID = setInterval(function () {
-	        //     //缓动
-	        //     _this.win.scrollTop += (tar - _this.win.scrollTop) * _this.f + 7*(_this.upOrDown === "up" ? -1 : 1);
-	        //     if (_this.upOrDown === 'up') {
-	        //     	if (tar >= _this.win.scrollTop) {
-		       //          clearInterval(_this.intervalID);
-		       //      }
-	        //     }else if(_this.upOrDown === 'dw'){
-	        //     	if (tar <= _this.win.scrollTop) {
-		       //          clearInterval(_this.intervalID);
-		       //      }
-	        //     }	           
-	        //     if (_this.win.scrollTop===0) clearInterval(_this.intervalID);
-	        //     // if (_this.win.scrollTop===5517) clearInterval(_this.intervalID);
-	        // }, _this.interval);
 
 	        $('body').stop().animate({scrollTop:tar},400);
 	        window.event.preventDefault();
